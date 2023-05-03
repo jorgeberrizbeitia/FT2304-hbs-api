77d960a1ef5523882fb9c16d0ced92ca39bd760b
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-05-03 16:10:16 +0200

    lecture done

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const port = 3000
 // configuraciones del servidor
 app.set('view engine', 'hbs');
 app.set("views", __dirname + "/views") // todas las vistas estarán en la carpeta views
+const hbs = require("hbs");
+hbs.registerPartials(__dirname + "/views/partials")
 
+
+const DogAPI = require("doggo-api-wrapper")
+const doggo = new DogAPI()
 
 // traernos la data de las lecciones
 const allLessons = require("./data/lessons-data.js")
@@ -69,10 +74,67 @@ app.get("/lessons/:bootcamp", (req, res) => {
   })
 
   res.render("lessons-per-bootcamp.hbs", {
-    lessons: filterArr
+    lessons: filterArr,
+    bootcamp: req.params.bootcamp
   })
 })
 
+app.get("/perritos/random", (req, res) => {
+
+  // Promesas
+  doggo.getARandomDog()
+  .then((response) => {
+    console.log(response)
+
+    res.render("dog/random.hbs", {
+      imageDog: response.message
+    })
+
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
+
+// ruta que me liste todas las razas de perritos
+app.get("/perritos/razas", (req, res) => {
+
+  doggo.getListOfAllBreeds()
+  .then((response) => {
+    console.log(response)
+    let allBreeds = Object.keys(response.message) 
+
+    res.render("dog/breed-list.hbs", {
+      allBreeds: allBreeds
+    })
+
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+  
+})
+
+// una ruta mostrar images de perritos por su raza.
+app.get("/perritos/por-raza/:breed", (req, res) => {
+
+  doggo.getAllDogsByBreed( req.params.breed )
+  .then((response) => {
+    console.log(response)
+
+    res.render("dog/dogs-by-breed.hbs", {
+      someDogs: response.message,
+      breed: req.params.breed
+    })
+
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
