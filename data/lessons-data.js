01aa5e66731545da123749a7ae437e8c8bc129ba
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-05-03 12:24:08 +0200

    hbs done

---
+++ data/lessons-data.js
@@ -0,0 +1,35 @@
+const lessons = [
+  {
+    topic: "Javascript",
+    description: "Don't use var",
+    bootcamp: "web",
+    approved: true,
+  },
+  {
+    topic: "Handlebars",
+    description: "It's like html... but better!",
+    bootcamp: "web",
+    approved: true,
+  },
+  {
+    topic: "Hacking 101",
+    description: "How to hack someone's facebook",
+    bootcamp: "cyber",
+    approved: false,
+  },
+  {
+    topic: "Designing",
+    description: "How to add proper colors",
+    bootcamp: "ux",
+    approved: false,
+  },
+  {
+    topic: "ChatGPT",
+    description: "How to ask chatGPT to do my taxes properly",
+    bootcamp: "cyber",
+    approved: true,
+  }
+]
+
+// exportar el modulo
+module.exports = lessons
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000
 app.set('view engine', 'hbs');
 app.set("views", __dirname + "/views") // todas las vistas estarán en la carpeta views
 
+
+// traernos la data de las lecciones
+const allLessons = require("./data/lessons-data.js")
+
 app.get('/', (req, res) => {
   // res.send('Hello World!')
 
@@ -26,6 +30,46 @@ app.get("/patata", (req, res) => {
   // debemos pasar siempre un Objeto
 })
 
+// ruta para mostrar todas las lecciones al cliente
+app.get("/lessons/all", (req, res) => {
+
+  // renderizar una vista
+  // pasarle las lecciones a la vista
+  console.log(allLessons)
+
+  res.render("all-lessons.hbs", {
+    lessons: allLessons
+  })
+
+})
+
+// ruta para mostrar unicamente las lecciones aprobadas
+app.get("/lessons/approved", (req, res) => {
+  
+  const filterArr = [];
+
+  allLessons.forEach((eachLesson) => {
+    if (eachLesson.approved === true) {
+      filterArr.push(eachLesson)
+    }
+  })
+
+  // const filterArr = allLessons.filter((eachLesson) => eachLesson.approved === true)
+
+  res.render("approved-lessons.hbs", {
+    lessons: filterArr
+  })
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// pasos para crear rutas
+
+// 1. crear la ruta sin nada adentro
+// 2. creamos una vista con una estructura base (sin contenido)
+// 3. renderizamos esta vista en la ruta y la probamos
+// 4. buscar la data y pasarla a la vista
+// 5. renderizar la data en el hbs
